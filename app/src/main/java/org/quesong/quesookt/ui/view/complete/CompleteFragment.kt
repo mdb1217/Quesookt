b49dfdc1152accd1f0234654ab0e84e730cb1f58
MIN-JU-CHO <60171052+MIN-JU-CHO@users.noreply.github.com>
2021-11-07 04:55:52 +0900

    [FEAT] #10 - QuestCompleteFragment

--- app/src/main/java/org/quesong/quesookt/ui/view/complete/CompleteFragment.kt
+++ app/src/main/java/org/quesong/quesookt/ui/view/complete/CompleteFragment.kt
@@ -5,9 +5,25 @@ import android.view.View
 import org.quesong.core.base.BindingFragment
 import org.quesong.quesookt.R
 import org.quesong.quesookt.databinding.FragmentCompleteBinding
+import org.quesong.quesookt.ui.view.complete.model.CompletedInfoData
 
 class CompleteFragment : BindingFragment<FragmentCompleteBinding>(R.layout.fragment_complete) {
+    private lateinit var completedQuestAdapter: CompletedQuestAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initCompletedAdapter()
+    }
+
+    private fun initCompletedAdapter(){
+        completedQuestAdapter = CompletedQuestAdapter()
+        binding.rvCompleted.adapter = completedQuestAdapter
+
+        completedQuestAdapter.completedList.addAll(
+            listOf(CompletedInfoData("타이틀","2000/01/01",""),
+                CompletedInfoData("타이틀","2000/01/01",""),
+                CompletedInfoData("타이틀","2000/01/01",""),
+                CompletedInfoData("타이틀","2000/01/01",""))
+        )
     }
 }
