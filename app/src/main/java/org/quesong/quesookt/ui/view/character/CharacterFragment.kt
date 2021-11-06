ad2f19c6d4d711a9e5f993891f958373ee27e23d
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-11-06 22:12:43 +0900

    [ADD] #1 - Add Character, Complete, QuestFragment

---
+++ app/src/main/java/org/quesong/quesookt/ui/view/character/CharacterFragment.kt
@@ -0,0 +1,13 @@
+package org.quesong.quesookt.ui.view.character
+
+import android.os.Bundle
+import android.view.View
+import org.quesong.core.base.BindingFragment
+import org.quesong.quesookt.R
+import org.quesong.quesookt.databinding.FragmentCharacterBinding
+
+class CharacterFragment : BindingFragment<FragmentCharacterBinding>(R.layout.fragment_character) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/org/quesong/quesookt/ui/view/quest/QuestFragment.kt
@@ -0,0 +1,13 @@
+package org.quesong.quesookt.ui.view.quest
+
+import android.os.Bundle
+import android.view.View
+import org.quesong.core.base.BindingFragment
+import org.quesong.quesookt.R
+import org.quesong.quesookt.databinding.FragmentQuestBinding
+
+class QuestFragment : BindingFragment<FragmentQuestBinding>(R.layout.fragment_quest) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
