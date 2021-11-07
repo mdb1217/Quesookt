0f4fa3f4c981a0bbb09da042d40b1455a5f78a7d
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-11-07 23:25:47 +0900

    [ADD] #24 - Add Complete Adapter

--- app/src/main/java/org/quesong/quesookt/ui/view/complete/CompletedQuestAdapter.kt
+++ app/src/main/java/org/quesong/quesookt/ui/view/complete/CompletedQuestAdapter.kt
@@ -3,13 +3,17 @@ package org.quesong.quesookt.ui.view.complete
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import org.quesong.core.util.extension.setImgFilter30
+import org.quesong.quesookt.R
+import org.quesong.quesookt.data.local.model.QuestData
 import org.quesong.quesookt.databinding.ItemCompletedQuestBinding
 import org.quesong.quesookt.ui.view.complete.model.CompletedInfoData
+import java.util.*
 
 class CompletedQuestAdapter : RecyclerView.Adapter<CompletedQuestAdapter.CompletedQuestViewHolder>() {
-    private val _completedList = mutableListOf<CompletedInfoData>()
+    private val _completedList = mutableListOf<QuestData>()
 
-    var completedList: MutableList<CompletedInfoData> = _completedList
+    var completedList: MutableList<QuestData> = _completedList
         set(value){
             _completedList.clear()
             _completedList.addAll(value)
@@ -28,8 +32,13 @@ class CompletedQuestAdapter : RecyclerView.Adapter<CompletedQuestAdapter.Complet
 
     override fun getItemCount(): Int = completedList.size
     class CompletedQuestViewHolder(private val binding: ItemCompletedQuestBinding) : RecyclerView.ViewHolder(binding.root){
-        fun onBind(completedInfoData: CompletedInfoData){
-            binding.completedInfoData = completedInfoData
+        fun onBind(questData: QuestData){
+            if(Random().nextInt(4) % 2 == 0)
+                binding.noondozang.setImageResource(R.drawable.ic_thumb)
+            else
+                binding.noondozang.setImageResource(R.drawable.ic_snow)
+            binding.ivQuest.setImgFilter30()
+            binding.completedInfoData = CompletedInfoData(questData.title, questData.date, questData.imageUrl)
         }
     }
 }
